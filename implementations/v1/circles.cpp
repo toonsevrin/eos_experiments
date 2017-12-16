73ea892f2046ad56920d95b439b7773d6b1ccfa3
toonsevrin <twan123@live.be>
2017-12-16 21:49:22 +0100

    Some improvements

--- implementations/v1/circles.cpp
+++ implementations/v1/circles.cpp
@@ -1,43 +1,72 @@
+#include <circles.hpp>
+
+namespace circles {
+    void createAccount(uint64_t account)
+    {
+        
+    }
+    void claimTokens(uint64_t account)
+    {
+          require_auth(account);
+    }
+    void trust(trust_relation relation)
+    {       
+          require_auth(relation.trustor);
+          eosio::print("TODO: Let ", eosio::name(relation.trustor),  " trust account ", eosio::name(relation.trustee), " \n");
+    }
+    void untrust(trust_relation relation)
+    {       
+          require_auth(relation.trustor);
+    }
+    void apply_exchange(exchange exchange_msg)
+    {
+        require_auth(exchange_msg.from);
+        eosio::print( "Transfer ", exchange_msg.amount, " from ", eosio::name(exchange_msg.from),"(", eosio::name(exchange_msg.from_currency), ") to ", eosio::name(exchange_msg.to), "(", eosio::name(exchange_msg.to_currency),  ")\n" );
+    }
+
+}
+
+using namespace circles;
+
 extern "C" {
 
-   void init() {
-      eosio::print( "Initializing Circles V1 contract\n" );
-   }
+    void init()  {
+       eosio::print( "Initializing Circles V1 contract\n" );
+    }
 
-
-   struct trust {
-       uint64_t target;
-   };
-
-   struct untrust {
-       uint64_t target;
-   };
-
-   struct exchange {
-       uint64_t from;
-       uint64_t to;
-       uint64_t amount;
-   };
-
-   /// The apply method implements the dispatch of events to this contract
-   void apply(uint64_t code, uint64_t action) {
-        if(action == N(newaccount)){
-            eosio::print("TODO: Create new account \n");
-        }else if(action == N(claim)){
-            eosio::print("TODO: CLaim currency \n");
-        }else if(action == N(trust)){
-            eosio::print("TODO: Trust account \n");
-        }else if(action == N(untrust)){
-            eosio::print("TODO: UNtrust account \n");
-        }else if(action == N(exchange)){
-            exchange message;
-            eosio::print("TODO: Exchange currency \n");
- static_assert(sizeof(message) == 3*sizeof(uint64_t), "unexpected padding");
-            auto read = read_message(&message, sizeof(message));
-            assert(read == sizeof(message), "message too short" );
-            eosio::print( "Transfer ", message.amount, " from ", eosio::name(message.from), " to ", eosio::name(message.to), "\n" );
-        }
-        eosio::print( "Received: ", eosio::name(code), "->", eosio::name(action), "\n" );
-   }
+/// The apply method implements the dispatch of events to this contract
+    void apply( uint64_t code, uint64_t action ) 
+    {
+         if(action == N(newaccount))
+	 {
+             eosio::print("TODO: Create new account \n");
+         }
+	 else if(action == N(claim))
+	 {
+	     eosio::print("TODO: CLaim currency \n");
+         }
+	 else if(action == N(trust))
+	 {
+             trust_relation message;
+	     static_assert(sizeof(message) == 2*sizeof(uint64_t), "unexpected padding");
+	     auto read = read_message(&message, sizeof(message));
+	     assert(read == sizeof(message), "message too short");
+             circles::trust(message);
+         }
+	 else if(action == N(untrust))
+	 {
+             eosio::print("TODO: UNtrust account \n");
+         }
+	 else if(action == N(exchange))
+	 {
+             exchange message;
+             eosio::print("TODO: Exchange currency \n");
+             static_assert(sizeof(message) == 5*sizeof(uint64_t), "unexpected padding");
+	     auto read = read_message(&message, sizeof(message));
+             assert(read == sizeof(message), "message too short");
+	     circles::apply_exchange(message);
+	 }
+         eosio::print( "Received: ", eosio::name(code), "->", eosio::name(action), "\n" );
+    }
 
 } // extern "C"
--- implementations/v1/circles.hpp
+++ implementations/v1/circles.hpp
@@ -1,6 +1,16 @@
-/**
- *  @file
- *  @copyright defined in eos/LICENSE.txt
- */
 #include <eoslib/eos.hpp>
 #include <eoslib/db.hpp>
+namespace circles {
+    struct trust_relation {
+            uint64_t trustor;
+            uint64_t trustee;
+    };
+
+    struct exchange {
+            uint64_t from;
+            uint64_t from_currency;
+            uint64_t to;
+            uint64_t to_currency;
+            uint64_t amount;
+        };
+}
